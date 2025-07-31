a530b11b74e53db7b021cc88cf92adb4cb62f9b2
Ali Mwabashiri <143745832+Abually@users.noreply.github.com>
2025-07-31 10:53:32 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,18 @@
+ # this is a simple calculator
+
+ #ask the user for input
+a= input("Enter the first value:")
+a= float(a)
+
+b = input("Enter the second value:")
+b = float(b)
+
+# perform calculations
+print(f"First value is: {a}")
+print(f"Second value is: {b}")
+print(f"Sum of the values is: ", a+b)
+print(f"Difference of the values is: ", a-b)
+print(f"Product of the values is: ", a*b)
+print(f"Quotient of the values is: ", a/b)
+print(f"Remainder of the values is: ", a%b)
+
